fbf3cb3c09d4860850860a96dd9b393f114fd9dd
19WH1A0513 <86731635+19WH1A0513@users.noreply.github.com>
2022-03-09 20:34:05 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
+import { DisplayComponent } from './display/display.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    HomePageComponent,
+    LoginComponent,
+    DisplayComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
