4d2c26ba0e92af1fbcf26595de35a67e932c465d
19WH1A1240 <69394589+19WH1A1240@users.noreply.github.com>
2022-03-17 11:07:15 +0530

    Update header.component.ts

--- header.component.ts
+++ header.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
+import { CartService } from '../cart.service';
+import { ActivatedRoute,Router } from '@angular/router';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor(public authService: AuthService){
-    
-  }
+  public totalItem : number = 0;
+  userDisplayName = '';
+  constructor(private route:ActivatedRoute, private router:Router,private service:CartService) { }
 
   ngOnInit(): void {
+    this.service.getFruits()
+    .subscribe(res=>{
+      this.totalItem = res.length;
+    })
   }
-
 }
