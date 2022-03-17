a500bda61d575d7641591b0ac3969b6d4007195c
vaishnavibilla <76248542+vaishnavibilla@users.noreply.github.com>
2022-03-17 15:09:13 +0530

    Create userhelp.component.ts

---
+++ userhelp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userhelp',
+  templateUrl: './userhelp.component.html',
+  styleUrls: ['./userhelp.component.css']
+})
+export class UserhelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
