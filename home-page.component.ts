a34527ec3d95a759f87707d404fdf6a542963c53
19WH1A0504 <86725809+19WH1A0504@users.noreply.github.com>
2022-03-17 11:10:35 +0530

    Update home-page.component.ts

--- home-page.component.ts
+++ home-page.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  items : any;
+  constructor() {
+    this.items=[
+      {ItemId:1,  Name:'APPLE',        price:20.00,q:'/piece',  imagePath:'./assets/Images/apple.png', ret:6},
+      {ItemId:2,  Name:'CAPSICUM',     price:40.00, q:'/kg',  imagePath:'./assets/Images/capsicum.png', ret:5},
+      {ItemId:3,  Name:'BANANA',       price:40.00,q:'/dozen',   imagePath:'./assets/Images/Banana.png', ret:2},
+      {ItemId:4,  Name:'CABBAGE',      price:40.00,q:'/kg',   imagePath:'./assets/Images/cabbage.png', ret:7},
+      {ItemId:5,  Name:'STRAWBERRY',   price:60.00,q:'/box of 6',   imagePath:'./assets/Images/Strawberry.jpg', ret:5},
+      {ItemId:6,  Name:'CARROT',       price:60.00,q:'/kg',   imagePath:'./assets/Images/carrot.png', ret:6},
+      {ItemId:7,  Name:'MANGOES',      price:70.00,q:'/kg',  imagePath:'./assets/Images/mangoes.png', ret:3},
+      {ItemId:8,  Name:'CORN',         price:14.00,q:'/piece',   imagePath:'./assets/Images/corn.png', ret:3},
+    ]
 
-  constructor() { }
+   }
 
   ngOnInit(): void {
+  }
+  addToCart(item: any): void {
+    console.log(item);
   }
 
 }
